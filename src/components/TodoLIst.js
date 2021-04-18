0bd8d9b1678e98283dc7a7c9906c88a4ba36d062
ifeoluwa5983 <o.ifeoluwah@gmail>
2021-04-19 00:38:28 +0100

    Todo app with react

---
+++ src/components/TodoLIst.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoLIst({todos, setTodos}) {
+    return (
+        <div className="todo-container">
+            <ul className= "todo-list">
+              {todos.map(todo => (
+                  <Todo setTodos={setTodos} todo={todo} todos={todos} key={todo.id} text={todo.text}  />
+              ))}
+            </ul>
+        </div>
+    )
+}
